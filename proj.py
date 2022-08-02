bafe54a434c5fb040fdbbbc4dfd9e5ec197cde69
vkallepa <vkallepa@uab.edu>
2022-08-01 23:45:39 -0500

    login done

--- proj.py
+++ proj.py
@@ -1,5 +1,8 @@
+from crypt import methods
+from email import message
 import boto3
 import os
+from django.shortcuts import render
 import psycopg2
 import json
 import datetime
@@ -15,21 +18,63 @@ app.secret_key = 'jfsahiwesdsdhks'
 
 @app.route('/',methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return render_template("signin.html")
+
+@app.route("/signup",methods=["GET","POST"])
+def signupIndex():
+    return render_template("signup.html")
+
+@app.route("/signin",methods=["GET","POST"])
+def signinIndex():
+    return render_template("signin.html")
+
+@app.route("/signupsubmit",methods=["GET","POST"])
+def signupsubmit():
+    f_name = request.form.get("f_name")
+    l_name = request.form.get("l_name")
+    u_email = request.form.get("u_email")
+    u_password = request.form.get("u_password")
+    print("f_name",f_name,"l_name",l_name,"email",u_email,"password",u_password)
+    db_conn = psycopg2.connect(
+        database="postgres", user='postgres', password='Deekshit2399', host='database-1-instance-1.cpfgthxswpv6.us-east-1.rds.amazonaws.com', port= '5432'
+    )
+    db_conn_cur = db_conn.cursor()
+    db_conn_cur.execute("""insert into users_data(first_name,last_name,email_id,password) values (%s,%s,%s,%s) """,(f_name,l_name,u_email,u_password))
+    db_conn.commit()
+    return render_template("signin.html")
+
+@app.route("/signinsubmit",methods=["GET","POST"])
+def signinsubmit():
+    u_email = str(request.form.get("u_email"))
+    u_password = request.form.get("u_password")
+    print(u_email,u_password)
+    db_conn = psycopg2.connect(
+        database="postgres", user='postgres', password='Deekshit2399', host='database-1-instance-1.cpfgthxswpv6.us-east-1.rds.amazonaws.com', port= '5432'
+    )
+    db_conn_cur = db_conn.cursor()
+    db_conn_cur.execute("""select * from users_data where email_id = '{email}' and password = '{password}' """.format(email = u_email,password =u_password))
+    password_matcher = len(db_conn_cur.fetchall()) > 0
+    print(password_matcher)
+    if password_matcher:
+        return render_template("index.html")
+    else:
+        message="Invalid Username or Password"
+        return render_template("signin.html",message=message)
+    
 
 @app.route("/submit",methods=["GET","POST"])
 def submitForm():
     file_upload_input = request.files["file_upload_input"]
-    file_upload_input.save(file_upload_input.filename)
+    file_upload_input.save(file_upload_input.filename.replace(" ",""))
     recipient_1 = request.form.get("recipient_1")
     recipient_2 = request.form.get("recipient_2")
     recipient_3 = request.form.get("recipient_3")
     recipient_4 = request.form.get("recipient_4")
     recipient_5 = request.form.get("recipient_5")
-    print(recipient_2)
-    # s3_upload_return = s3.upload_file(file_upload_input.filename,"bucketproj",file_upload_input.filename)
+    print(file_upload_input.filename.replace(" ",""))
+    s3_upload_return = s3.upload_file(file_upload_input.filename,"bucketproj",file_upload_input.filename.replace(" ",""))
     email_client = boto3.client('ses',region_name="us-east-1")
-    s3_file_link = "https://bucketproj.s3.amazonaws.com/{}".format(file_upload_input.filename)
+    s3_file_link = "https://bucketproj.s3.amazonaws.com/{}".format(file_upload_input.filename.replace(" ",""))
     body = """Please click on the below link to download the file
     
             {}
@@ -37,31 +82,31 @@ def submitForm():
     emails_list = [recipient_1]
     if recipient_2:
         emails_list = [recipient_1,recipient_2]
-    elif recipient_3:
+    if recipient_3:
         emails_list = [recipient_1,recipient_2,recipient_3]
-    elif recipient_4:
+    if recipient_4:
         emails_list = [recipient_1,recipient_2,recipient_3,recipient_4]
-    elif recipient_5:
+    if recipient_5:
         emails_list = [recipient_1,recipient_2,recipient_3,recipient_4,recipient_5]
     print(emails_list)
-    # email_client.send_email(
-    #     Source = 'vkallepa@uab.edu',
-	#     Destination = {
-	# 	    'ToAddresses': emails_list
-	#     },
-	#     Message = {
-	# 	    'Subject': {
-	# 		    'Data': 'Project File Upload',
-	# 		    'Charset': 'UTF-8'
-	# 	    },
-	# 	    'Body': {
-	# 		    'Text':{
-	# 			    'Data': body,
-	# 			    'Charset': 'UTF-8'
-	# 		    }
-	# 	    }
-	#     }
-    # )
+    email_client.send_email(
+        Source = 'vkallepa@uab.edu',
+	    Destination = {
+		    'ToAddresses': emails_list
+	    },
+	    Message = {
+		    'Subject': {
+			    'Data': 'Project File Upload',
+			    'Charset': 'UTF-8'
+		    },
+		    'Body': {
+			    'Text':{
+				    'Data': body,
+				    'Charset': 'UTF-8'
+			    }
+		    }
+	    }
+    )
     db_conn = psycopg2.connect(
         database="postgres", user='postgres', password='Deekshit2399', host='database-1-instance-1.cpfgthxswpv6.us-east-1.rds.amazonaws.com', port= '5432'
     )
